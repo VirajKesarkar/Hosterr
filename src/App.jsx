4f1455187c5bd209afaf52bca3fd90185a78ce9c
VirajKesarkar <162833324+VirajKesarkar@users.noreply.github.com>
2025-07-28 00:27:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from"./Components/Header"
+import Body from"./Components/Body"
+import Footer from"./Components/Footer"
+function App() {
+ return (
+   <div className="bg-yellow-200 min-h-screen px-6 py-7  flex flex-col gap-10">
+    <Header />
+    <Body />
+    <Footer />
+    
+
+   </div>
+
+
+  )
+}
+
+export default App;
